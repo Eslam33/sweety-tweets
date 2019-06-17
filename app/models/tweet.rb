385e482f00e9bbd434aa2fd64149c5d47761701e
Eslam <eslam7amdy2011@gmail.com>
2019-06-17 03:44:22 -0700

    More validations & fix some bugs

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,6 @@
 class Tweet < ApplicationRecord
 	belongs_to :user
+
+    validates :tweet, presence: true ,  length: { minimum: 1 } ,  length: { maximum: 255 }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
    has_many :tweets
+
+   validates :name, presence: true ,  length: { minimum: 2 } ,  length: { maximum: 24 }, format: { with: /\A[a-zA-Z0-9]+\Z/ }
+   validates :username, presence: true , uniqueness: true , length: { minimum: 2 } ,  length: { maximum: 24 } , format: { with: /\A[a-zA-Z0-9]+\Z/ }
+   validates :email, presence: true , uniqueness: true , format: { with: URI::MailTo::EMAIL_REGEXP } 
+   validates :password, presence: true , length: { in: 6..20 } , confirmation: true
+   validates :password_confirmation, presence: true
+
+
 end
